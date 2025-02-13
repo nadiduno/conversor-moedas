2db6aca41e7e0a97ecd8068129098cf90d2938f3
Nadi Duno <nadiduno@gmail.com>
2025-02-13 00:46:57 -0400

    project

---
+++ src/main/java/com/conversor/moeda/MoedaApplication.java
@@ -0,0 +1,13 @@
+package com.conversor.moeda;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoedaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MoedaApplication.class, args);
+	}
+
+}
